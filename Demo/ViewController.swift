406415c9e7c0accac5d0268a5b2802cff34109a9
Rashid Latif <rashid.latif93@gmail.com>
2020-08-08 18:09:21 +0400

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Rashid Latif on 08/08/2020.
+//  Copyright © 2020 Cmall biz llc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
